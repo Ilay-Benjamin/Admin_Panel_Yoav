ca5dcdf7a923671b3b2b1e9e0053fb6e06e5ac8f
Ilay-Benjamin <ilaypro1222@gmail.com>
2024-08-24 18:55:50 +0300

    Created ContactsEditors

--- src/pages/editors/ContactEditor/index.ContactEditor.js
+++ src/pages/editors/ContactEditor/index.ContactEditor.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes, BrowserRouter, Link, Outlet, Redirect } from 'react-router-dom';
+import classNames from 'classnames';
+import saveIcon from '../../../assets/images/icons/save.png';
+import deleteIcon from '../../../assets/images/icons/delete.png';
+import editIcon from '../../../assets/images/icons/edit.png';
+import cancleIcon from '../../../assets/images/icons/cancel.png';
+import addDepartmentIcon from '../../../assets/images/icons/add_department.png';
+import addContactIcon from '../../../assets/images/icons/add-contact.png';
+
+import './style.ContactEditor.css';
+
+
 
 function ContactEditor() {
   return (
-    <div className="page" style={{margin: '0', padding: '0'}}>
-      <h1 className='page-title'>ContactEditor Page</h1>
-      <h3 style={{textDecoration: 'underline'}}>
-        /editors
-      </h3>
-      <br></br>
-      <p className='page-description' style={{padding: 15, fontSize: 20, backgroundColor: 'lightgray', borderRadius: 10, border: '1px solid black'}}>
-        adff
-      </p>
+    <div className="page" style={{ margin: '0', padding: '0' }}>
+      <div className='page-title'>
+        <p className='page-title-text'>
+          ContactEditor Page
+        </p>
+      </div>
+      <div className='page-content'>
+
+        <div className={classNames('editor', 'disabled-editor')}>
+
+          <div className='editor-header'>
+            <div className={classNames('editor-button', 'editor-header-button')}>
+              <img src={addDepartmentIcon} className={classNames('editor-button-image', 'header-button-image')} alt=""></img>
+            </div>
+          </div>
+
+          <div className='editor-board'>
+            <div className='editor-board-header'>
+              <div className={classNames('board-header-button')}>
+                <img src={saveIcon} className={classNames('board-button-image')} alt=""></img>
+              </div>
+              <div className={classNames('board-header-button')}>
+                <img src={cancleIcon} className={classNames('board-button-image')} alt=""></img>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
